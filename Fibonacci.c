4e83f58bc3a5e56bc2655aaaad09301d966ab6ac
dancerjk16-cloud <dancer.jk16@gmail.com>
2026-02-16 10:42:55 +0530

    Update Fibonacci.c

---
+++ Fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n, a = 0, b = 1, c;
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+    printf("Fibonacci Series: ");
+    for (int i = 0; i < n; i++) {
+        if (i <= 1) {
+            c = i;
+        } else {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        printf("%d ", c);
+    }
+    printf("\n");
+    return 0;
+}
